a472e32bc70028038e52fdd81ea9fde6493fa7db
capslol19 <maxim.balov19@gmail.com>
2023-06-23 01:00:26 +0200

    add sidebar + catalog with swapi

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -9,10 +9,10 @@ const SideBar = ({onSelectCategory}) => {
         <div className='sidebar'>
             <form className="sidebar__search-bar"><input type="text"/></form>
             <ul className="sidebar__menu">
-                <li onClick={ () => onSelectCategory(1)} className="sidebar__menu-item">1</li>
-                <li onClick={ () => onSelectCategory(2)} className="sidebar__menu-item">2</li>
-                <li onClick={ () => onSelectCategory(3)} className="sidebar__menu-item">3</li>
-                <li onClick={ () => onSelectCategory(4)} className="sidebar__menu-item">4</li>
+                <li onClick={() => onSelectCategory(1)} className="sidebar__menu-item">1</li>
+                <li onClick={() => onSelectCategory(2)} className="sidebar__menu-item">2</li>
+                <li onClick={() => onSelectCategory(3)} className="sidebar__menu-item">3</li>
+                <li onClick={() => onSelectCategory(4)} className="sidebar__menu-item">4</li>
             </ul>
 
         </div>
